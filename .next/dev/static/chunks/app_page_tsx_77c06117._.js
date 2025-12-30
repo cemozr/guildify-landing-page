2879f8ea9a9b9eb7faa6556263603e60ce2a6f9f
Cem <98841937+cemozr@users.noreply.github.com>
2025-12-30 22:41:32 +0300

    Remove deprecated cache life type definitions, route types, and validator types from Next.js

--- .next/dev/static/chunks/app_page_tsx_77c06117._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_423b2b61._.js",
-  "static/chunks/node_modules_faf18b8d._.js"
-],
-    source: "dynamic"
-});
